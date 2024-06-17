3f332f374a1cc142d5c80b4a015bcb84e9edc003
Kauaninhakrs <111157690+Kauaninhakrs@users.noreply.github.com>
2024-06-17 09:56:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Como a inclusão de estudos culturais no currículo escolar pode impactar os alunos?",
+        alternativas: [
+            {
+                texto: "Incluir estudos culturais no currículo escolar!",
+                afirmacao: "Desenvolvimento da compreensão mais profunda e respeitosa  das diversas culturas"
+            },
+            {
+                texto: "Promover atividades extracurriculares relacionados à cultura!",
+                afirmacao: "Engajamento e motivação em sua aprendizagem, visto que se sentem mais conectados com o conteúdo"
+            }
+        ]
+    },
+];  
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+
+mostraPergunta();
